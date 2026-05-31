6e78cd88852f62e6d17d588f35688df8d1f15c21
Pgriha <pgrihar@gmail.com>
2026-05-31 05:24:26 +0300

    misc things

--- Content.Shared/_RMC14/Weapons/Ranged/HoloTargeting/HoloTargetingComponent.cs
+++ Content.Shared/_RMC14/Weapons/Ranged/HoloTargeting/HoloTargetingComponent.cs
@@ -15,7 +15,7 @@ public sealed partial class HoloTargetingComponent : Component
     ///     this amount of stacks or more.
     /// </summary>
     [DataField]
-    public float MaxStacks = 300f; // Sich. 100 в оригіналі. 30% замість 10%
+    public float MaxStacks = 350f; // Sich. 100 в оригіналі. 35% замість 10%
 
     /// <summary>
     ///     The decay rate of the applied holo stacks
