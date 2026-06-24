59fa73e23f012844114778976877f9bd39d6efcf
Pgriha <pgrihar@gmail.com>
2026-06-24 07:01:30 +0300

    tests №2

--- Content.IntegrationTests/Tests/PostMapInitTest.cs
+++ Content.IntegrationTests/Tests/PostMapInitTest.cs
@@ -517,7 +517,7 @@ namespace Content.IntegrationTests.Tests
 
             var gameMaps = protoManager.EnumeratePrototypes<GameMapPrototype>().Select(o => o.MapPath).ToHashSet();
 
-            var mapFolder = new ResPath("/Maps/_Sich") // Mriya
+            var mapFolder = new ResPath("/Maps/_Sich"); // Mriya
             var maps = resourceManager
                 .ContentFindFiles(mapFolder)
                 .Where(filePath => filePath.Extension == "yml" && !filePath.Filename.StartsWith(".", StringComparison.Ordinal))
